8cc2ec7b11f151a49846b56fd583af9cf682c661
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-03-28 13:51:21 +1100

    Install npm  axios, UI is completed

--- src/component/form/SearchForm.js
+++ src/component/form/SearchForm.js
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
 import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { CustomCard } from "../card/CustomCard";
 
 export const SearchForm = () => {
+  const [search, setSearch] = useState("");
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+  };
+
+  const handleOnsubmit = (e) => {
+    e.preventDefault();
+
+    alert("got the search value, now go get movie from api");
+  };
+
   return (
     <>
       <Form>
         <Row>
           <Col>
-            <Form.Control placeholder="Input movie title" />
+            <Form.Control
+              placeholder="Search..."
+              onChange={handleOnChange}
+              required
+            />
 
             <Button variant="warning" type="submit">
               Search{" "}
